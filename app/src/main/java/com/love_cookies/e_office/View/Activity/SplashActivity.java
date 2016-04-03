8fdc3e40ebe49a936428c8d530217aba7404a372
cookie <tsubasa-kun@foxmail.com>
2016-04-03 23:23:27 +0800

    添加应用启动页

---
+++ app/src/main/java/com/love_cookies/e_office/View/Activity/SplashActivity.java
@@ -0,0 +1,53 @@
+package com.love_cookies.e_office.View.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.KeyEvent;
+import android.view.View;
+
+import com.love_cookies.cookie_library.Activity.BaseActivity;
+import com.love_cookies.e_office.R;
+
+import org.xutils.view.annotation.ContentView;
+
+/**
+ * Created by xiekun on 2016/04/03.
+ *
+ * APP启动页
+ */
+@ContentView(R.layout.activity_splash)
+public class SplashActivity extends BaseActivity {
+
+	private final int SPLASH_DISPLAY_DURATION = 1500;//启动页显示时长
+	Looper looper = Looper.myLooper();
+	private Handler handler = new Handler(looper);
+
+	Runnable runnable = new Runnable() {
+		@Override
+		public void run() {
+			Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+			startActivity(intent);
+			finish();
+		}
+	};
+
+	@Override
+	public void initWidget(Bundle savedInstanceState) {
+		handler.postDelayed(runnable, SPLASH_DISPLAY_DURATION);
+	}
+
+	@Override
+	public void widgetClick(View v) {
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			handler.removeCallbacks(runnable);
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+}
