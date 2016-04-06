32b56eb770ab03cf1e5734d48ec51830146e4571
Cookie <tsubasa-kun@foxmail.com>
2016-04-06 10:57:33 +0800

    启动页添加背景

--- app/src/main/java/com/love_cookies/e_office/View/Activity/SplashActivity.java
+++ app/src/main/java/com/love_cookies/e_office/View/Activity/SplashActivity.java
@@ -5,11 +5,14 @@ import android.os.Handler;
 import android.os.Looper;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.love_cookies.cookie_library.Activity.BaseActivity;
 import com.love_cookies.e_office.R;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
 
 /**
  * Created by xiekun on 2016/04/03.
@@ -18,6 +21,9 @@ import org.xutils.view.annotation.ContentView;
  */
 @ContentView(R.layout.activity_splash)
 public class SplashActivity extends BaseActivity {
+
+	@ViewInject(R.id.splash_bg_iv)
+	ImageView splashBgIV;
 
 	private final int SPLASH_DISPLAY_DURATION = 1500;//启动页显示时长
 	Looper looper = Looper.myLooper();
@@ -32,6 +38,7 @@ public class SplashActivity extends BaseActivity {
 
 	@Override
 	public void initWidget(Bundle savedInstanceState) {
+		x.image().bind(splashBgIV, "assets://splash_bg.png");
 		handler.postDelayed(runnable, SPLASH_DISPLAY_DURATION);
 	}
 
